7b2ea4c92ea703fadcff7fcbc37c63cf7d204681
Paula Origlio <minplynn@aol.com>
2015-10-19 12:25:13 -0600

    added factory

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
 angular.module("pizzaApp",[])
 
-angular.module("pizzaApp").controller("pizzaController",["$scope","$http",function($scope,$http){
+angular.module("pizzaApp").controller("pizzaController",["$scope","$http","pizzaFactory",function($scope,$http,pizzaFactory){
 
 	$scope.greeting = "pizza + thing = thing pizza"
+
+	$scope.pizzas = pizzaFactory.pizzas
 
 }])
--- public/js/pizzafactory.js
+++ public/js/pizzafactory.js
@@ -2,8 +2,20 @@ angular.module("pizzaApp").factory("pizzaFactory",function(){
 
 	var pizzas = []
 
+	var Pizza = function(name,price,description,toppings,glutenFree,picture){
+		this.name = name
+		this.price = price
+		this.description = description
+		this.toppings = toppings
+		this.glutenFree = glutenFree
+		this.picture = picture
+		pizzas.push(this)
+	}
 
+	var thriftPizza = new Pizza("Thrift Store Pizza",3.99,"This pizza combines none of what you love about pizza with ALL of what you love about your local Salvation Army! On dough we got from the King Soopers markdown aisle, we spread mismatched buttons and bric-a-brac over a fine bed of VHS tape spaghetti, straight from a first-run copy of the Land Before Time.",["bric-a-brac","VHS tape","buttons","socks","half-used nail polish jars"],false,"http://www.chic-steals.com/wp-content/uploads/2011/02/goodwillbins11.jpg")
 
-
+	return {
+		pizzas : pizzas
+	}
 
 })
